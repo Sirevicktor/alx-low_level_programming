56f32695db6482b1b5dcf67fdd5905ca29fd2bab
Sirevicktor <victorubeng@gmail.com>
2023-02-23 23:34:16 -0800

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,6 +6,7 @@
 * followed by a new line
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 long int n;
@@ -20,10 +21,9 @@ while (n % 2 == 0)
 max = 2;
 n /= 2;
 }
-
 for (i = 3; i <= sqrt(n); i = i + 2)
 {
- while (n % i == 0)
+while (n % i == 0)
 {
 max = i;
 n = n / i;
@@ -34,7 +34,6 @@ if (n > 2)
 max = n;
 
 printf("%ld\n", max);
-
 return (0);
 }
 
